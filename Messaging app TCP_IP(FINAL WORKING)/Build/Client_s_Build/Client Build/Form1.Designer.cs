8e7779170ac147638edf6ffe9139a0d319b88a29
Nico Deng <88329510+AC3R4PT0R@users.noreply.github.com>
2023-05-10 00:13:19 +0200

    Add files via upload
    Working Server(Dashboard) and multi user

---
+++ Messaging app TCP_IP(FINAL WORKING)/Build/Client_s_Build/Client Build/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Client_Build
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rch_txt_messages = new System.Windows.Forms.RichTextBox();
+            this.btn_connect_to_server = new System.Windows.Forms.Button();
+            this.btn_send_message = new System.Windows.Forms.Button();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client_Build";
+            // 
+            // rch_txt_messages
+            // 
+            this.rch_txt_messages.Location = new System.Drawing.Point(156, 109);
+            this.rch_txt_messages.Name = "rch_txt_messages";
+            this.rch_txt_messages.Size = new System.Drawing.Size(364, 334);
+            this.rch_txt_messages.TabIndex = 1;
+            this.rch_txt_messages.Text = "";
+            // 
+            // btn_connect_to_server
+            // 
+            this.btn_connect_to_server.Location = new System.Drawing.Point(611, 130);
+            this.btn_connect_to_server.Name = "btn_connect_to_server";
+            this.btn_connect_to_server.Size = new System.Drawing.Size(109, 23);
+            this.btn_connect_to_server.TabIndex = 2;
+            this.btn_connect_to_server.Text = "Connect to Server";
+            this.btn_connect_to_server.UseVisualStyleBackColor = true;
+            this.btn_connect_to_server.Click += new System.EventHandler(this.btn_connect_to_server_Click);
+            // 
+            // btn_send_message
+            // 
+            this.btn_send_message.Location = new System.Drawing.Point(611, 473);
+            this.btn_send_message.Name = "btn_send_message";
+            this.btn_send_message.Size = new System.Drawing.Size(89, 23);
+            this.btn_send_message.TabIndex = 3;
+            this.btn_send_message.Text = "Send Message";
+            this.btn_send_message.UseVisualStyleBackColor = true;
+            this.btn_send_message.Click += new System.EventHandler(this.btn_send_message_Click);
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(156, 475);
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(276, 20);
+            this.txt_message.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(611, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 605);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.btn_send_message);
+            this.Controls.Add(this.btn_connect_to_server);
+            this.Controls.Add(this.rch_txt_messages);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rch_txt_messages;
+        private System.Windows.Forms.Button btn_connect_to_server;
+        private System.Windows.Forms.Button btn_send_message;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Messaging app TCP_IP(FINAL WORKING)/Build/Server_Build/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Build
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TcpClient Client;
+        TcpListener Server;
+        NetworkStream Stream;
+        string host = "127.0.0.1";
+        int port = 4444;
+        bool ServerStatus = false;
+        private bool isServerRunning = false;
+        public class ClientHandler
+        {
+            public TcpClient Client { get; private set; }
+            public NetworkStream Stream { get; private set; }
+
+            public ClientHandler(TcpClient client)
+            {
+                Client = client;
+                Stream = client.GetStream();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (ServerStatus == false)
+            {
+                lbl_server_status.Text = "Server Status:  Offline";
+            }
+            else 
+            {
+                lbl_server_status.Text = "Server Status:  Online";
+            }
+            lbl_clients.Text += "Clients : 0";
+            lbl_server_ip.Text += host;
+            lbl_server_port.Text += port.ToString();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseConnection();
+        }
+
+        private async void btn_Start_Server_Click(object sender, EventArgs e)
+        {
+            ServerStatus = true;
+            Server = new TcpListener(IPAddress.Any, port);
+            Server.Start();
+
+            ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Waiting for a client to connect..." + Environment.NewLine));
+
+            isServerRunning = true;
+
+            if (ServerStatus == false)
+            {
+                lbl_server_status.Text = "Server Status:  Offline";
+            }
+            else
+            {
+                lbl_server_status.Text = "Server Status:  Online";
+            }
+
+            while (isServerRunning)
+            {
+                try
+                {
+                    TcpClient newClient = await Server.AcceptTcpClientAsync();
+                    ClientHandler clientHandler = new ClientHandler(newClient);
+                    Task.Run(() => HandleClient(clientHandler));
+                    string clientIp = ((IPEndPoint)newClient.Client.RemoteEndPoint).Address.ToString();
+                    int clientPort = ((IPEndPoint)newClient.Client.RemoteEndPoint).Port;
+                    ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Connected to client" + Environment.NewLine));
+                    ControlInvoke(comboBoxClients, () => comboBoxClients.Items.Add(clientIp + ":" + clientPort.ToString()));
+
+                    // Display a message box to indicate that a client has connected
+                    MessageBox.Show("A client has connected to the server.");
+                }
+                catch (Exception ex)
+                {
+                    if (isServerRunning)
+                    {
+                        ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Error accepting client: " + ex.Message + Environment.NewLine));
+                    }
+                }
+            }
+        }
+        private void onConnect(IAsyncResult AR)
+        {
+            Client = Server.EndAcceptTcpClient(AR);
+            Stream = Client.GetStream();
+            byte[] buffer = new byte[1024];
+            Stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(onReceive), buffer);
+            ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Connected to client" + Environment.NewLine));
+
+            // Display a message box to indicate that a client has connected
+            MessageBox.Show("A client has connected to the server.");
+
+            // Listen for incoming data from the client
+            while (true)
+            {
+                try
+                {
+                    buffer = new byte[1024];
+                    int bytesRead = Stream.Read(buffer, 0, buffer.Length);
+                    ControlInvoke(rch_text_log, () => rch_text_log.AppendText(">>: " + Encoding.UTF8.GetString(buffer, 0, bytesRead) + Environment.NewLine));
+                }
+                catch (Exception ex)
+                {
+                    ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Error receiving data: " + ex.Message + Environment.NewLine));
+                    break;
+                }
+            }
+        }
+        private void onReceive(IAsyncResult AR)
+        {
+            try
+            {
+                int bytesRead = Stream.EndRead(AR);
+                if (bytesRead > 0)
+                {
+                    byte[] buffer = (byte[])AR.AsyncState;
+                    ControlInvoke(rch_text_log, () => rch_text_log.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + ((IPEndPoint)Client.Client.RemoteEndPoint).ToString() + " (To Server): " + Encoding.UTF8.GetString(buffer, 0, bytesRead) + Environment.NewLine));
+                    Stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(onReceive), buffer);
+                }
+            }
+            catch (Exception ex)
+            {
+                ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Error receiving data: " + ex.Message + Environment.NewLine));
+            }
+
+        }
+
+        private async void btn_server_sendmessage_Click(object sender, EventArgs e)
+        {
+            if (Client == null || !Client.Connected)
+            {
+                ControlInvoke(rch_text_log, () => rch_text_log.AppendText("No client connected" + Environment.NewLine));
+                return;
+            }
+
+            try
+            {
+                byte[] message = Encoding.UTF8.GetBytes(txt_message.Text);
+                await Stream.WriteAsync(message, 0, message.Length);
+                ControlInvoke(rch_text_log, () => rch_text_log.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " Server (To " + ((IPEndPoint)Client.Client.RemoteEndPoint).ToString() + "): " + txt_message.Text + Environment.NewLine));
+
+                txt_message.Clear();
+            }
+            catch (Exception ex)
+            {
+                ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Error sending data: " + ex.Message + Environment.NewLine));
+            }
+        }
+        delegate void UniversalVoidDelegate();
+
+        public static void ControlInvoke(Control control, Action function)
+        {
+
+            if (control.IsDisposed || control.Disposing)
+            {
+                return;
+            }
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new UniversalVoidDelegate(() => ControlInvoke(control, function)));
+                return;
+            }
+            function();
+        }
+        private void CloseConnection()
+        {
+            if (Client != null && Client.Connected)
+            {
+                Stream.Close();
+                Client.Close();
+            }
+
+            if (Server != null)
+            {
+                Server.Stop();
+            }
+        }
+
+        private void btn_stop_server_Click(object sender, EventArgs e)
+        {
+            ServerStatus = false;
+            try
+            {
+                isServerRunning = false;
+
+                if (Server != null)
+                {
+                    Server.Stop();
+                    ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Server stopped." + Environment.NewLine));
+                    if (ServerStatus == false)
+                    {
+                        lbl_clients.Text = "Clients : 0";
+                        lbl_server_status.Text = "Server Status:  Offline";
+                    }
+                }
+
+                if (Stream != null)
+                {
+                    Stream.Close();
+                }
+
+                if (Client != null)
+                {
+                    Client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                ControlInvoke(rch_text_log, () => rch_text_log.AppendText("Error stopping server: " + ex.Message + Environment.NewLine));
+            }
+        }
+        private async void HandleClient(ClientHandler clientHandler)
+        {
+            try
+            {
+                NetworkStream streamHandler = clientHandler.Stream;
+                byte[] buffer = new byte[1024];
+
+                while (true)
+                {
+                    int bytesRead = await streamHandler.ReadAsync(buffer, 0, buffer.Length);
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    // Process the received message
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error or display a message
+            }
+        }
+    }
+}
